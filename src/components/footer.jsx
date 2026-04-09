1a44e97f62be0125a52c8e366e2d93a98bb215f8
NdY1717 <omg203mane@gmail.com>
2026-04-09 16:44:48 -0600

    Creación de Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+'use client'
+export default function Footer() {
+
+    return (
+    <footer>
+        <div>
+            
+            <p>Universidad Autonoma del Estado de Hidalgo pendeja</p>
+            <p>Licenciatura en Ciencias Computacionales Pendeja</p>
+            <p>Bases de datos distribuidas pendeja</p>
+            <p>6° 1</p>
+        </div>
+
+        <div>
+            <p>Integrantes:</p>
+            <p>Brandom Galder Hernandez Franco Pendejo</p>
+            <p>Mario André Lozada Alfaro Pendejo</p>
+            <p>Mauricio Daniel Manzanilla Pornung Pendejo</p>
+            <p>Felipe de Jesus Orta Escobar Pendejo</p>
+        </div>
+    </footer>
+    );
+}
